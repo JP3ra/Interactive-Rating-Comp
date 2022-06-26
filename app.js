2f53df58bcc026625e0b24cbf47f9792d6c69bba
Jyotiprakash Panda <92199359+JP3ra@users.noreply.github.com>
2022-06-26 11:04:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let userRating = '';
+
+document.querySelector('.option1').onclick = function(){
+   userRating = '1';
+   location.href="thanks1.html"
+}
+
+document.querySelector('.option2').onclick = function(){
+
+    location.href="thanks2.html";
+}
+
+
+document.querySelector('.option3').onclick = function(){
+    userRating='3';
+    location.href="thanks3.html";
+
+}
+
+
+document.querySelector('.option4').onclick = function(){
+    userRating='4';
+    location.href="thanks4.html"
+}
+
+
+document.querySelector('.option5').onclick = function(){
+    userRating = '5';
+    location.href="thanks5.html"
+}
+
+
+
+
+
+
+
+/**let userRating = 0;
+
+document.getElementsByClassName(".option").**/
+
+
+
